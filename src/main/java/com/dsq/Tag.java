f87a55803eb940c9cc767a9bfadadfcadab82268
大邓 <971203251@qq.com>
2025-10-11 18:35:17 +0800

    feat: implement Tag entity

---
+++ src/main/java/com/dsq/Tag.java
@@ -0,0 +1,66 @@
+package com.dsq;
+
+import java.util.Objects;
+
+/**
+ * 标签类
+ * 用于对笔记进行分类和标记
+ */
+public class Tag {
+    private Long id;
+    private String name;
+
+    // 构造方法
+    public Tag() {
+    }
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public Tag(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getter 和 Setter 方法
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // 重写 equals 和 hashCode 方法，基于 id 和 name
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(id, tag.id) &&
+                Objects.equals(name, tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    // 重写 toString 方法
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
